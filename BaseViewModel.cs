fe1d804124bfd969fb8c1c1737e10ce54a145996
AnghelAndrei28 <45268599+AnghelAndrei28@users.noreply.github.com>
2019-07-23 13:47:17 +0300

    Example of the preference page

---
+++ BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ImageButtonSample
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        protected BaseViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ImageButtonSampleViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ImageButtonSample
+{
+    public class ImageButtonSampleViewModel : BaseViewModel
+    {
+        public static ImageButtonSampleViewModel Instance { get; } = new ImageButtonSampleViewModel();
+
+        public ImageButtonSampleViewModel()
+        {
+            ToggleLockStateCommand = new Command(() => ToggleLockState());
+        }
+
+        private void ToggleLockState()
+        {
+            Locked = !Locked;
+        }
+
+        public ICommand ToggleLockStateCommand { get; private set; }
+
+        private bool _locked = false;
+        public bool Locked
+        {
+            get => _locked;
+            set
+            {
+                if (_locked != value)
+                {
+                    Debug.WriteLine($"Locked: {value}");
+
+                    _locked = value;
+
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
